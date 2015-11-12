6063166d77267e8f1d1ceb0f26f2e1c964bdc355
astrofra <astrofra@gmail.com>
2015-11-12 22:48:37 +0100

    - Torus mesh (needs a zsort).

--- 3d_meshes/src/main.c
+++ 3d_meshes/src/main.c
@@ -2,7 +2,7 @@
 
 #include "meshs.h"
 
-#define MAX_POINTS  256
+#define MAX_POINTS  512
 
 
 Vect3D_f16 pts_3D[MAX_POINTS];
@@ -71,10 +71,10 @@ int main()
     rotstep.y = FIX16(0.05);
 
     // set the current mesh
-    mesh_coord = cylinder_coord;
-    mesh_poly_ind = cylinder_poly_ind;
-    mesh_line_ind = cylinder_line_ind;
-    mesh_face_norm = cylinder_face_norm;
+    mesh_coord = cylinder_subd_coord;
+    mesh_poly_ind = cylinder_subd_poly_ind;
+    mesh_line_ind = cylinder_subd_line_ind;
+    mesh_face_norm = cylinder_subd_face_norm;
 
     while (1)
     {
@@ -112,9 +112,9 @@ int main()
 void updatePointsPos()
 {
     // transform 3D point
-    M3D_transform(&transformation, mesh_coord, pts_3D, 54);
+    M3D_transform(&transformation, mesh_coord, pts_3D, CYLINDER_SUBD_VTX_COUNT);
     // project 3D point (f16) to 2D point (s16)
-    M3D_project_s16(pts_3D, pts_2D, 54);
+    M3D_project_s16(pts_3D, pts_2D, CYLINDER_SUBD_VTX_COUNT);
 }
 
 void drawPoints(u8 col)
@@ -129,7 +129,7 @@ void drawPoints(u8 col)
     norm = mesh_face_norm;
     poly_ind = mesh_poly_ind;
 
-    i = 16;
+    i = CYLINDER_SUBD_FACE_COUNT;
 
     while (i--)
     {
--- 3d_meshes/src/meshs.h
+++ 3d_meshes/src/meshs.h
@@ -1,10 +1,28 @@
+#define CUBE_VTX_COUNT 24
+#define CUBE_FACE_COUNT 6
 const Vect3D_f16 cube_coord[24];
 const u16  cube_poly_ind[24];
 const u16  cube_line_ind[16];
 const Vect3D_f16  cube_face_norm[6];
 
+#define CYLINDER_VTX_COUNT 54
+#define CYLINDER_FACE_COUNT 16
 const Vect3D_f16 cylinder_coord[54];
 const u16  cylinder_poly_ind[64];
 const u16  cylinder_line_ind[50];
 const Vect3D_f16  cylinder_face_norm[16];
+
+#define CYLINDER_SUBD_VTX_COUNT 240
+#define CYLINDER_SUBD_FACE_COUNT 80
+const Vect3D_f16 cylinder_subd_coord[240];
+const u16  cylinder_subd_poly_ind[320];
+const u16  cylinder_subd_line_ind[192];
+const Vect3D_f16  cylinder_subd_face_norm[80];
+
+#define CYLINDER_SUBD_2_VTX_COUNT 1158
+#define CYLINDER_SUBD_2_FACE_COUNT 384
+const Vect3D_f16 cylinder_subd_2_coord[1158];
+const u16  cylinder_subd_2_poly_ind[1536];
+const u16  cylinder_subd_2_line_ind[848];
+const Vect3D_f16  cylinder_subd_2_face_norm[384];
 
