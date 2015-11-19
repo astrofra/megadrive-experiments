0059a654abe8a26a5fbe18243406ba1a8f27f1b6
astrofra <astrofra@gmail.com>
2015-11-19 07:53:34 +0100

    - 3D routine cleanup & variables refactoring.

--- 3d_meshes/src/main.c
+++ 3d_meshes/src/main.c
@@ -17,10 +17,6 @@ extern Mat3D_f16 Mat;
 Vect3D_f16 pts_3D[MAX_POINTS];
 Vect2D_s16 pts_2D[MAX_POINTS];
 
-Rotation3D rotation;
-Translation3D translation;
-Transformation3D transformation;
-
 Vect3D_f16 rotstep;
 
 fix16 camdist;
@@ -34,8 +30,6 @@ const Vect3D_f16 *mesh_face_norm;
 
 struct  QSORT_ENTRY poly_zsort[metacube_FACE_COUNT];
 
-void updatePointsPos();
-void drawPoints(u8 col);
 void handleJoyEvent(u16 joy, u16 changed, u16 state);
 void RSE_3DFlatShadedScreen(void);
 
@@ -62,74 +56,15 @@ int main()
 	RSE_3DFlatShadedScreen();
 }
 
-void RSE_3DFlatShadedScreen(void)
-{
-    char str[16];
-
-    camdist = FIX16(15);
-
-    M3D_reset();
-    M3D_setCamDistance(camdist);
-    M3D_setLightEnabled(1);
-    M3D_setLightXYZ(FIX16(0.9), FIX16(0.9), FIX16(-0.9));
-
-    // allocate translation and rotation structure
-    M3D_setTransform(&transformation, &translation, &rotation);
-    M3D_setTranslation(&transformation, FIX16(0), FIX16(0), FIX16(20));
-    M3D_setRotation(&transformation, FIX16(0.0), FIX16(0.0), FIX16(0.0));
-
-    rotstep.x = FIX16(0.05);
-    rotstep.y = FIX16(0.05);
-
-    // set the current mesh
-    PART_3D_LOAD_MESH(metacube);
-    // mesh_coord = metacube_coord;
-    // mesh_poly_ind = metacube_poly_ind;
-    // mesh_line_ind = metacube_line_ind;
-    // mesh_face_norm = metacube_face_norm;
-
-    zsort_switch = 0;
-
-    while (1)
-    {
-        M3D_setCamDistance(camdist);
-
-        // do work here
-        rotation.x += rotstep.x;
-        rotation.y += rotstep.y;
-        rotation.z += rotstep.z;
-        transformation.rebuildMat = 1;
-
-        updatePointsPos();
-
-        // ensure previous flip buffer request has been started
-        BMP_waitWhileFlipRequestPending();
-        BMP_showFPS(1);
-
-        BMP_clear();
-
-        drawPoints(0xFF);
-
-        BMP_drawText("trans z:", 0, 2);
-        fix16ToStr(translation.z, str, 2);
-        BMP_drawText(str, 10, 2);
-        BMP_drawText("cam dist:", 0, 3);
-        fix16ToStr(camdist, str, 2);
-        BMP_drawText(str, 11, 3);
-
-        BMP_flip(1);
-    }	
-}
-
-void updatePointsPos()
+void inline updatePointsPos(Transformation3D *transformation)
 {
     // transform 3D point
-    M3D_transform(&transformation, mesh_coord, pts_3D, metacube_VTX_COUNT);
+    M3D_transform(transformation, mesh_coord, pts_3D, metacube_VTX_COUNT);
     // project 3D point (f16) to 2D point (s16)
     M3D_project_s16(pts_3D, pts_2D, metacube_VTX_COUNT);
 }
 
-void drawPoints(u8 col)
+void inline drawPoints(u8 col, Transformation3D *transformation)
 {
     Vect2D_s16 v[4];
     const Vect3D_f16 *norm;
@@ -183,9 +118,9 @@ void drawPoints(u8 col)
         {
         	//	Compute the lighting of the polygon
 	        norm = &mesh_face_norm[poly_zsort[i].index];
-            dp = fix16Mul(transformation.lightInv.x, norm->x) +
-				fix16Mul(transformation.lightInv.y, norm->y) +
-				fix16Mul(transformation.lightInv.z, norm->z);
+            dp = fix16Mul(transformation->lightInv.x, norm->x) +
+				fix16Mul(transformation->lightInv.y, norm->y) +
+				fix16Mul(transformation->lightInv.z, norm->z);
 
 	        if (dp > 0) col += (dp >> (FIX16_FRAC_BITS - 2));
 	            BMP_drawPolygon(v, 4, col | (col << 4));
@@ -202,4 +137,63 @@ void handleJoyEvent(u16 joy, u16 changed, u16 state)
             // flatDrawing = 1 - flatDrawing;
         }
     }
+}
+
+void RSE_3DFlatShadedScreen(void)
+{
+    char str[16];
+
+    Rotation3D rotation;
+	Translation3D translation;
+	Transformation3D transformation;
+
+    camdist = FIX16(15);
+
+    M3D_reset();
+    M3D_setCamDistance(camdist);
+    M3D_setLightEnabled(1);
+    M3D_setLightXYZ(FIX16(0.9), FIX16(0.9), FIX16(-0.9));
+
+    // allocate translation and rotation structure
+    M3D_setTransform(&transformation, &translation, &rotation);
+    M3D_setTranslation(&transformation, FIX16(0), FIX16(0), FIX16(20));
+    M3D_setRotation(&transformation, FIX16(0.0), FIX16(0.0), FIX16(0.0));
+
+    rotstep.x = FIX16(0.05);
+    rotstep.y = FIX16(0.05);
+
+    // set the current mesh
+    PART_3D_LOAD_MESH(metacube);
+
+    zsort_switch = 0;
+
+    while (1)
+    {
+        M3D_setCamDistance(camdist);
+
+        // do work here
+        rotation.x += rotstep.x;
+        rotation.y += rotstep.y;
+        rotation.z += rotstep.z;
+        transformation.rebuildMat = 1;
+
+        updatePointsPos(&transformation);
+
+        // ensure previous flip buffer request has been started
+        BMP_waitWhileFlipRequestPending();
+        BMP_showFPS(1);
+
+        BMP_clear();
+
+        drawPoints(0xFF, &transformation);
+
+        BMP_drawText("trans z:", 0, 2);
+        fix16ToStr(translation.z, str, 2);
+        BMP_drawText(str, 10, 2);
+        BMP_drawText("cam dist:", 0, 3);
+        fix16ToStr(camdist, str, 2);
+        BMP_drawText(str, 11, 3);
+
+        BMP_flip(1);
+    }	
 }
