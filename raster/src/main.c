3d16e928cbbf03025b86c638a6aa998578ef8741
David Vandensteen <dvandensteen@gmail.com>
2015-12-01 17:55:58 +0100

    Formatting

--- raster/src/main.c
+++ raster/src/main.c
@@ -2,13 +2,13 @@
 #include <gfx.h>
 #define CHAR_SIZE 4
 
-
-
 int main(){
 	u16 hInterruptCounter = 0;
 	u32 hscroll = 0;
 	u32 hscrollInc = 0x30;
 	u16 vblCount = 0;
+	char hInterruptCounterStr[CHAR_SIZE];
+	u16 vramIndex = TILE_USERINDEX;
 
 	void hBlank(){
 		hInterruptCounter++;
@@ -20,26 +20,17 @@ int main(){
 			hscroll -= hscrollInc;
 		}
 	}
-
 	VDP_clearPlan(APLAN, 0);
 	VDP_clearPlan(BPLAN, 0);
 	VDP_setScreenWidth320();
-
-	char hInterruptCounterStr[CHAR_SIZE];
-	u16 vramIndex = TILE_USERINDEX;
-
 	SYS_disableInts();
 	VDP_setPalette(PAL0, car_image.palette->data);
 	VDP_drawImageEx(APLAN, &car_image, TILE_ATTR_FULL(PAL0, TRUE, FALSE, FALSE, vramIndex), 1, 15, FALSE, FALSE);
 	vramIndex += car_image.tileset->numTile;
 	SYS_enableInts();
-
 	VDP_drawText("H INTERRUPTION COUNTER :", 0, 5);
-
-	SYS_enableInts();
 	VDP_setHInterrupt(1);
 	SYS_setHIntCallback(&hBlank); //hBlank function is called on each h interruption
-
 	while (1){
 		VDP_waitVSync();
 		vblCount++;
