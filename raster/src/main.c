9c9e13c5302df2417654e79c4e7833ec200eaa3e
David Vandensteen <dvandensteen@gmail.com>
2015-12-01 17:18:58 +0100

    First visual raster effect

--- raster/src/main.c
+++ raster/src/main.c
@@ -1,46 +1,49 @@
 #include <genesis.h>
-#include <kdebug.h>
+#include <gfx.h>
 #define CHAR_SIZE 4
 
 u16 hInterruptCounter = 0;
 u32 hscroll = 0;
+u32 hscrollInc = 0x30;
+u16 vblCount = 0;
 
 void hBlank(){
-	hInterruptCounter++;	
+	hInterruptCounter++;
 	VDP_setHorizontalScroll(PLAN_A, hscroll >> 8);
-	//hscroll += 0x30;
-	hscroll += 10;
+	if (hInterruptCounter % 2 == 0){
+		hscroll += hscrollInc;
+	}
+	else{
+		hscroll -= hscrollInc;
+	}
 }
 
 int main(){
-	VDP_clearPlan(APLAN,0);
-	VDP_clearPlan(BPLAN,0);
+	VDP_clearPlan(APLAN, 0);
+	VDP_clearPlan(BPLAN, 0);
 	VDP_setScreenWidth320();
+
 	char hInterruptCounterStr[CHAR_SIZE];
-	
+	u16 vramIndex = TILE_USERINDEX;
+
+	SYS_disableInts();
+	VDP_setPalette(PAL0, car_image.palette->data);
+	VDP_drawImageEx(APLAN, &car_image, TILE_ATTR_FULL(PAL0, TRUE, FALSE, FALSE, vramIndex), 1, 15, FALSE, FALSE);
+	vramIndex += car_image.tileset->numTile;
+	SYS_enableInts();
+
 	VDP_drawText("H INTERRUPTION COUNTER :", 0, 5);
 
 	SYS_enableInts();
 	VDP_setHInterrupt(1);
 	SYS_setHIntCallback(&hBlank); //hBlank function is called on each h interruption
-	
+
 	while (1){
-		//VDP_waitVSync();
-		VDP_setHorizontalScroll(PLAN_A, 0);
-		hscroll = 0;
-		KDebug_Alert("SGDK HCOUNTER");
-		KDebug_AlertNumber(GET_HCOUNTER);
-
-		KDebug_Alert("SGDK VCOUNTER");
-		KDebug_AlertNumber(GET_VCOUNTER);
-
-		KDebug_Alert("SGDK HVCOUNTER");
-		KDebug_AlertNumber(GET_HVCOUNTER);
-
-		uintToStr(hInterruptCounter, hInterruptCounterStr, CHAR_SIZE);
-		VDP_drawText(hInterruptCounterStr, 0, 10);		
-		KDebug_Alert("----------------------------");
 		VDP_waitVSync();
+		vblCount++;
+		hscrollInc += 0x8;
+		uintToStr(hInterruptCounter, hInterruptCounterStr, CHAR_SIZE);
+		VDP_drawText(hInterruptCounterStr, 0, 10);
 	}
 	return 0;
 }
