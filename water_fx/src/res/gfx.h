8011fbfd2ec0942db338a21374cb9aad7eca7953
astrofra <astrofra@gmail.com>
2015-12-02 22:53:45 +0100

    - Added 2 bitmaps for the "water fx" screen.

---
+++ water_fx/src/res/gfx.h
@@ -0,0 +1,7 @@
+#ifndef _GFX_H_
+#define _GFX_H_
+
+extern const Image water_tex_back;
+extern const Image water_tex_front;
+
+#endif // _GFX_H_
---
+++ water_fx/src/res/gfx.res
@@ -0,0 +1,2 @@
+IMAGE water_tex_back "water_texture_back_layer.png" -1
+IMAGE water_tex_front "water_texture_front_layer.png" -1
