613fa833dd5ae1642505d935d6cf09949687535a
astrofra <astrofra@gmail.com>
2015-12-06 23:23:40 +0100

    - Draft vector ball fx, improved the rotation (2 axis).

--- vector_balls/src/ball_coords.h
+++ vector_balls/src/ball_coords.h
@@ -2,5 +2,7 @@
 const Vect3D_f16 vb_cube_vertex_pos[8];
 #define grid_cube_VTX_COUNT 100
 const Vect3D_f16 vb_grid_cube_vertex_pos[100];
+#define grid_cube_small_VTX_COUNT 27
+const Vect3D_f16 vb_grid_cube_small_vertex_pos[27];
 #define grid_sphere_VTX_COUNT 84
 const Vect3D_f16 vb_grid_sphere_vertex_pos[84];
--- vector_balls/src/main.c
+++ vector_balls/src/main.c
@@ -4,6 +4,8 @@
 #include "ball_coords.h"
 
 #define	MAX_VECTOR_BALL 256
+#define BALL_COUNT grid_cube_small_VTX_COUNT
+#define VECTOR_BALL_ARRAY vb_grid_cube_small_vertex_pos
 
 static void vectorBallFX();
 
@@ -13,30 +15,46 @@ int main(){
 	return 0;
 }
 
-void drawVectorBalls(Sprite *sprites, u16 angle)
+void drawVectorBalls(Sprite *sprites, u16 rx, u16 ry)
 {
 	u16 loop;
 	short x, y;
 	Vect3D_f16 _vtx, t_vtx;
-	fix16 _sin, _cos;
+	fix16 _cosx, _sinx, _cosy, _siny, cs, cc, ss, sc;
 	u16 x_screen, y_screen;
 	x_screen = (VDP_getScreenWidth() - 32) >> 1;
 	y_screen = (VDP_getScreenHeight() - 32) >> 1;
 
-	_cos = cosFix16(angle);
-	_sin = sinFix16(angle);
+	_cosx = cosFix16(rx);
+	_sinx = sinFix16(rx);
+	_cosy = cosFix16(ry);
+	_siny = sinFix16(ry);
+	cs = fix16Mul(_cosx, _siny);
+	ss = fix16Mul(_siny, _sinx);
+	cc = fix16Mul(_cosx, _cosy);
+	sc = fix16Mul(_sinx, _cosy);
 
-	for(loop = 0; loop < cube_VTX_COUNT; loop++)
+	for(loop = 0; loop < BALL_COUNT; loop++)
 	{
-		_vtx = vb_cube_vertex_pos[loop];
+		_vtx = VECTOR_BALL_ARRAY[loop];
 
-		//	x' = x cos f - y sin f
-		t_vtx.x = fix16Sub(fix16Mul(_vtx.x, _cos), fix16Mul(_vtx.y, _sin));
-		//	y' = y cos f + x sin f
-		t_vtx.y = fix16Add(fix16Mul(_vtx.y, _cos), fix16Mul(_vtx.x, _sin));
+		//	1D rotation (on Y axis)
+		/*
+			//	x' = x cos f - y sin f
+			t_vtx.x = fix16Sub(fix16Mul(_vtx.x, _cosy), fix16Mul(_vtx.y, _siny));
+			//	y' = y cos f + x sin f
+			t_vtx.y = fix16Add(fix16Mul(_vtx.y, _cosy), fix16Mul(_vtx.x, _siny));
+			t_vtx.z = _vtx.z;
+		*/
 
+		//	2D rotation (on X and Y axis)
+	    t_vtx.x = fix16Mul(_vtx.x, _sinx) + fix16Mul(_vtx.y, _cosx);
+	    t_vtx.y = fix16Mul(_vtx.x, cs) - fix16Mul(_vtx.y, ss) + fix16Mul(_vtx.z, _cosy);
+	    t_vtx.z = fix16Mul(_vtx.x, cc) - fix16Mul(_vtx.y, sc) - fix16Mul(_vtx.z, _siny);
+
+	    //	Isometric 2D projection
 		x = t_vtx.x;
-		y = t_vtx.y + (_vtx.z >> 1);
+		y = t_vtx.y + (t_vtx.z >> 1);
 
 		x >>= 3;
 		y >>= 3;
@@ -44,7 +62,7 @@ void drawVectorBalls(Sprite *sprites, u16 angle)
 		SPR_setPosition(&sprites[loop], x_screen + x, y_screen + y);
 	}
 
-	SPR_update(sprites, cube_VTX_COUNT);
+	SPR_update(sprites, BALL_COUNT);
 }
 
 static void vectorBallFX(){
@@ -61,10 +79,10 @@ static void vectorBallFX(){
 	VDP_setPalette(PAL2, ball_metal.palette->data);
 	SPR_init(MAX_VECTOR_BALL);
 
-	for(loop = 0; loop < cube_VTX_COUNT; loop++)
+	for(loop = 0; loop < BALL_COUNT; loop++)
 	    SPR_initSprite(&sprites[loop], &ball_metal, 0, 0, TILE_ATTR_FULL(PAL2, TRUE, FALSE, FALSE, 0));
 	// SPR_setPosition(&sprites[0], (VDP_getScreenWidth() - 32) >> 1, (VDP_getScreenHeight() - 32) >> 1);
-    SPR_update(sprites, cube_VTX_COUNT);
+    SPR_update(sprites, BALL_COUNT);
 
 	SYS_enableInts();
 
@@ -72,7 +90,7 @@ static void vectorBallFX(){
 
 	while (TRUE){
 		VDP_waitVSync();
-		drawVectorBalls(sprites, angle);
-		angle = (angle + 1) & (FP_COSINE_TABLE_LEN - 1);
+		drawVectorBalls(sprites, angle & (FP_COSINE_TABLE_LEN - 1), (angle << 1) & (FP_COSINE_TABLE_LEN - 1));
+		angle++;
 	}
 }
