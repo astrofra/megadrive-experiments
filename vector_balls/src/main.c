ce8e1dcb3a6b52bf027613e8194d5ffb9ae6a1c5
astrofra <astrofra@gmail.com>
2015-12-06 22:50:21 +0100

    - Draft vector ball fx, obvious optimizations.

--- vector_balls/src/main.c
+++ vector_balls/src/main.c
@@ -18,23 +18,30 @@ void drawVectorBalls(Sprite *sprites, u16 angle)
 	u16 loop;
 	short x, y;
 	Vect3D_f16 _vtx, t_vtx;
+	fix16 _sin, _cos;
+	u16 x_screen, y_screen;
+	x_screen = (VDP_getScreenWidth() - 32) >> 1;
+	y_screen = (VDP_getScreenHeight() - 32) >> 1;
+
+	_cos = cosFix16(angle);
+	_sin = sinFix16(angle);
 
 	for(loop = 0; loop < cube_VTX_COUNT; loop++)
 	{
 		_vtx = vb_cube_vertex_pos[loop];
 
 		//	x' = x cos f - y sin f
-		t_vtx.x = fix16Sub(fix16Mul(_vtx.x, fp_cos[angle]), fix16Mul(_vtx.y, fp_sin[angle]));
+		t_vtx.x = fix16Sub(fix16Mul(_vtx.x, _cos), fix16Mul(_vtx.y, _sin));
 		//	y' = y cos f + x sin f
-		t_vtx.y = fix16Add(fix16Mul(_vtx.y, fp_cos[angle]), fix16Mul(_vtx.x, fp_sin[angle]));
+		t_vtx.y = fix16Add(fix16Mul(_vtx.y, _cos), fix16Mul(_vtx.x, _sin));
 
-		x = fix16Int(t_vtx.x);
-		y = fix16Int(t_vtx.y + (_vtx.z >> 2));
+		x = t_vtx.x;
+		y = t_vtx.y + (_vtx.z >> 1);
 
 		x >>= 3;
 		y >>= 3;
 
-		SPR_setPosition(&sprites[loop], ((VDP_getScreenWidth() - 32) >> 1) + x, ((VDP_getScreenHeight() - 32) >> 1) + y);
+		SPR_setPosition(&sprites[loop], x_screen + x, y_screen + y);
 	}
 
 	SPR_update(sprites, cube_VTX_COUNT);
