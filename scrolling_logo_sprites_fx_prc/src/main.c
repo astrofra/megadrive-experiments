83e2a1093f74e9cef4373d72807c280e731525ba
astrofra <astrofra@gmail.com>
2015-12-17 08:03:14 +0100

    - Tiny intro project : fine tuned the scrolling.

--- scrolling_logo_sprites_fx_prc/src/main.c
+++ scrolling_logo_sprites_fx_prc/src/main.c
@@ -4,6 +4,7 @@
 #include "sprites_traj.h"
 
 #define SPRITE_COUNT 50
+#define HBLANK_STEP 50
 
 static void beastScrollingFX();
 
@@ -24,12 +25,10 @@ static void beastScrollingFX(){
 	/*	Hblank-based water fx */
 	static void hBlank(){
 		hscrollInc++;
-
-		if (hscrollInc <= 1)
-			VDP_setHorizontalScroll(PLAN_B, -vblCount);
+		if (hscrollInc < (200 / HBLANK_STEP))
+			VDP_setHorizontalScroll(PLAN_A, sinFix16(vblCount << 2));
 		else
-		if (hscrollInc >= 15)
-			VDP_setHorizontalScroll(PLAN_B, 0);
+			VDP_setHorizontalScroll(PLAN_A, 0);
 	}
 
 	SYS_disableInts();
@@ -46,13 +45,13 @@ static void beastScrollingFX(){
 
 	/* Draw the foreground */
 	VDP_setPalette(PAL1, ground.palette->data);
-	VDP_drawImageEx(BPLAN, &ground, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, vramIndex), 0, 4, FALSE, FALSE);
-	VDP_drawImageEx(BPLAN, &ground, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, vramIndex), 24, 4, FALSE, FALSE);
+	VDP_drawImageEx(BPLAN, &ground, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, vramIndex), 0, 2, FALSE, FALSE);
+	VDP_drawImageEx(BPLAN, &ground, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, vramIndex), 24, 2, FALSE, FALSE);
 	vramIndex += ground.tileset->numTile;
 
 	/* Draw the logo */
 	VDP_setPalette(PAL0, rse_logo.palette->data);
-	VDP_drawImageEx(APLAN, &rse_logo, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, vramIndex), 0, 8, FALSE, FALSE);
+	VDP_drawImageEx(APLAN, &rse_logo, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, vramIndex), 0, 7, FALSE, FALSE);
 	vramIndex += rse_logo.tileset->numTile;	    
 
 	for(i = 0; i < SPRITE_COUNT; i++)
@@ -63,7 +62,7 @@ static void beastScrollingFX(){
 
 	SYS_enableInts();
 
-	VDP_setHIntCounter(15);
+	VDP_setHIntCounter(HBLANK_STEP);
 	VDP_setHInterrupt(1);
 	SYS_setHIntCallback(&hBlank); //hBlank function is called on each h interruption	
 
@@ -72,8 +71,8 @@ static void beastScrollingFX(){
 	while (1){
 		hscrollInc = 0;
 		VDP_waitVSync();
-		// VDP_setHorizontalScroll(PLAN_B, -vblCount);
-		VDP_setHorizontalScroll(PLAN_A, sinFix16(vblCount << 2));
+		VDP_setHorizontalScroll(PLAN_B, -vblCount);
+		// VDP_setHorizontalScroll(PLAN_A, sinFix16(vblCount << 2));
 
 		tmp_spr_traj = spr_traj + ((vblCount << 1) & (SPRT_TABLE_LEN - 1));
 		for(i = 0; i < SPRITE_COUNT; i++)
@@ -85,7 +84,7 @@ static void beastScrollingFX(){
 
 		SPR_update(sprites, SPRITE_COUNT);
 
-		VDP_setTileMapXY(VDP_PLAN_B, TILE_USERINDEX + vblCount, vblCount, 20);
+		VDP_setTileMapXY(VDP_PLAN_A, TILE_USERINDEX + vblCount, vblCount, 22);
 
 		vblCount += 1;
 	}
