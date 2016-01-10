f4d70275055114f9b59893900305ba0e89f87ff5
astrofra <astrofra@gmail.com>
2016-01-10 12:49:43 +0100

    - Dot cube FX, using sprites and a macro instead of a function, to gain ~4fps.

--- fast_dot_cube/src/main.c
+++ fast_dot_cube/src/main.c
@@ -2,7 +2,7 @@
 #include <gfx.h>
 #include "edges.h"
 
-#define	MAX_VECTOR_BALL 80
+#define	MAX_VECTOR_BALL 64
 #define BALL_COUNT cube_VTX_COUNT
 #define VECTOR_BALL_ARRAY vb_cube_vertex_pos
 
@@ -13,31 +13,29 @@ int main(){
 	return 0;
 }
 
+#define DRAW_MIDDLE_SPRITE(idx, x1, y1, x2, y2) \
+{	\
+	u16 x3, y3;	\
+	x3 = (x1 + x2) >> 1;	\
+	y3 = (y1 + y2) >> 1;	\
+	\
+    sprites[idx].x = x3;	\
+    sprites[idx].y = y3;	\
+    idx++;	\
+	\
+    sprites[idx].x = (x1 + x3) >> 1;	\
+    sprites[idx].y = (y1 + y3) >> 1;	\
+    idx++;	\
+	\
+    sprites[idx].x = (x2 + x3) >> 1;	\
+    sprites[idx].y = (y2 + y3) >> 1;	\
+    idx++;	\
+};
+
 static void fastCubeFX(){
 	u16 loop, j;
 	Sprite sprites[MAX_VECTOR_BALL];
 	u16 angle = 0;
-
-	static u16 inline drawMiddleSprite(u16 idx, u16 x1, u16 y1, u16 x2, u16 y2)
-	{
-		u16 x3, y3;
-		x3 = (x1 + x2) >> 1;
-		y3 = (y1 + y2) >> 1;
-
-        sprites[idx].x = x3;
-        sprites[idx].y = y3;
-        idx++;
-
-        sprites[idx].x = (x1 + x3) >> 1;
-        sprites[idx].y = (y1 + y3) >> 1;
-        idx++;
-
-        sprites[idx].x = (x2 + x3) >> 1;
-        sprites[idx].y = (y2 + y3) >> 1;
-        idx++;
-
-        return idx;
-	}
 
 	static void drawDots(Sprite *sprites, u16 rx, u16 ry)
 	{
@@ -99,21 +97,22 @@ static void fastCubeFX(){
 		}
 
 		/* Face #0 */
-		loop = drawMiddleSprite(loop, t_vtx_2d[0].x, t_vtx_2d[0].y, t_vtx_2d[1].x, t_vtx_2d[1].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[1].x, t_vtx_2d[1].y, t_vtx_2d[2].x, t_vtx_2d[2].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[2].x, t_vtx_2d[2].y, t_vtx_2d[3].x, t_vtx_2d[3].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[0].x, t_vtx_2d[0].y, t_vtx_2d[3].x, t_vtx_2d[3].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[0].x, t_vtx_2d[0].y, t_vtx_2d[1].x, t_vtx_2d[1].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[1].x, t_vtx_2d[1].y, t_vtx_2d[2].x, t_vtx_2d[2].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[2].x, t_vtx_2d[2].y, t_vtx_2d[3].x, t_vtx_2d[3].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[0].x, t_vtx_2d[0].y, t_vtx_2d[3].x, t_vtx_2d[3].y);
 
 		/* Face #1 */
-		loop = drawMiddleSprite(loop, t_vtx_2d[4].x, t_vtx_2d[4].y, t_vtx_2d[5].x, t_vtx_2d[5].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[6].x, t_vtx_2d[6].y, t_vtx_2d[5].x, t_vtx_2d[5].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[6].x, t_vtx_2d[6].y, t_vtx_2d[7].x, t_vtx_2d[7].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[4].x, t_vtx_2d[4].y, t_vtx_2d[7].x, t_vtx_2d[7].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[4].x, t_vtx_2d[4].y, t_vtx_2d[5].x, t_vtx_2d[5].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[6].x, t_vtx_2d[6].y, t_vtx_2d[5].x, t_vtx_2d[5].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[6].x, t_vtx_2d[6].y, t_vtx_2d[7].x, t_vtx_2d[7].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[4].x, t_vtx_2d[4].y, t_vtx_2d[7].x, t_vtx_2d[7].y);
 
-		loop = drawMiddleSprite(loop, t_vtx_2d[0].x, t_vtx_2d[0].y, t_vtx_2d[4].x, t_vtx_2d[4].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[1].x, t_vtx_2d[1].y, t_vtx_2d[5].x, t_vtx_2d[5].y);
-		loop = drawMiddleSprite(loop, t_vtx_2d[2].x, t_vtx_2d[2].y, t_vtx_2d[6].x, t_vtx_2d[6].y);
-		// loop = drawMiddleSprite(loop, t_vtx_2d[3].x, t_vtx_2d[3].y, t_vtx_2d[7].x, t_vtx_2d[7].y);
+		/* Vertical edges */
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[0].x, t_vtx_2d[0].y, t_vtx_2d[4].x, t_vtx_2d[4].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[1].x, t_vtx_2d[1].y, t_vtx_2d[5].x, t_vtx_2d[5].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[2].x, t_vtx_2d[2].y, t_vtx_2d[6].x, t_vtx_2d[6].y);
+		DRAW_MIDDLE_SPRITE(loop, t_vtx_2d[3].x, t_vtx_2d[3].y, t_vtx_2d[7].x, t_vtx_2d[7].y);
 
 		/* Update the whole list of sprites */
 		SPR_update(sprites, loop);
@@ -127,7 +126,7 @@ static void fastCubeFX(){
 	/* Set the palette taken from the vector ball sprite */
 	VDP_setPalette(PAL2, fast_cube_dot.palette->data);
 	SPR_init(MAX_VECTOR_BALL);
-// 
+
 	/*	Initialize the needed amount of sprites */
 	for(loop = 0; loop < MAX_VECTOR_BALL; loop++)
 	    SPR_initSprite(&sprites[loop], &fast_cube_dot, 0, 0, TILE_ATTR_FULL(PAL2, TRUE, FALSE, FALSE, 0));
