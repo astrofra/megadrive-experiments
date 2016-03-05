83a9845307249784511777fa94ee544fb4a0cb4a
astrofra <astrofra@gmail.com>
2016-03-05 09:32:08 +0100

    - New "underwater starfield" fx.

---
+++ underwater_starfield_fx/src/res/gfx.h
@@ -0,0 +1,7 @@
+#ifndef _GFX_H_
+#define _GFX_H_
+
+extern const SpriteDefinition sprite_stars;
+extern const Image underwater;
+
+#endif // _GFX_H_
---
+++ underwater_starfield_fx/src/res/gfx.res
@@ -0,0 +1,2 @@
+SPRITE sprite_stars "sprite_stars.png" 1 1 -1
+IMAGE underwater "underwater.png" -1
