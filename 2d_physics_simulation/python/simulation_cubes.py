8a413562dd0d92963c77d87384d1468032082fd2
astrofra <astrofra@gmail.com>
2016-04-08 07:51:09 +0200

    - 2D physics simulation, working simulation & export draft.

--- 2d_physics_simulation/python/simulation_cubes.py
+++ 2d_physics_simulation/python/simulation_cubes.py
@@ -9,7 +9,7 @@ from math import pi, cos, sin, asin, radians
 import codecs
 
 filename_out			=	"../src/simulation"
-scale_factor			=	5.0
+scale_factor			=	1.0
 
 md_screen_w             =   320/10.0
 md_screen_h             =   220/10.0
@@ -23,17 +23,15 @@ scn = scene.new_scene()
 scn.GetPhysicSystem().SetDefaultRigidBodyAxisLock(gs.LockZ)
 scn.GetPhysicSystem().SetDebugVisuals(True)
 
-cam = scene.add_camera(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 0.0, -10.0)))
+cam = scene.add_camera(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(0, 0.0, -40.0)))
 
-# screen = scene.add_plane(scn, mat=gs.Matrix4.TransformationMatrix(gs.Vector3(0,0,0), gs.Vector3(radians(-90),0,0)), width=md_screen_w, depth=md_screen_h)
+screen = scene.add_plane(scn, mat=gs.Matrix4.TransformationMatrix(gs.Vector3(0,0,0), gs.Vector3(radians(-90),0,0)), width=md_screen_w, depth=md_screen_h)
 
 scene.add_light(scn, gs.Matrix4.RotationMatrix(gs.Vector3(0.65, -0.45, 0)), gs.Light.Model_Linear, 150)
 scene.add_light(scn, gs.Matrix4.RotationMatrix(gs.Vector3(0.55, pi, 0.2)), gs.Light.Model_Linear, diffuse=gs.Color(0.3, 0.3, 0.4))
-scene.add_physic_plane(scn, mat=gs.Matrix4.TransformationMatrix(gs.Vector3(0,-md_screen_h / 30,0), gs.Vector3(0,0,0)))
+scene.add_physic_plane(scn, mat=gs.Matrix4.TransformationMatrix(gs.Vector3(0,-md_screen_h / 2,0), gs.Vector3(0,0,0)))
 
-# nodes = add_kapla_tower(scn, 0.5, 2, 2, 6, 16)
-
-width = 0.5
+sphere_radius = md_screen_w / 40.0
 
 node_list = []
 rb_list = []
@@ -41,9 +39,9 @@ stream_list = []
 
 
 def make_solid_pos(x,y):
-	return gs.Vector3(x * 1.15, y + width * 0.5, cos(x * y + x) * 0.15)
+	return gs.Vector3(x * 1.15, y + sphere_radius * 0.5, 0.0)
 
-cube_masks = [	[0,0,1,0],
+cube_masks = [	[1,0,1,1],
 				[0,1,1,0],
 				[1,1,1,1]]
 
@@ -51,7 +49,7 @@ for y in range(3):
 	for x in range(-2, 2):
 		if cube_masks[y][x + 2] == 1:
 			world = gs.Matrix4.TransformationMatrix(make_solid_pos(x,y), gs.Vector3(0, pi * 0.25 * cos(x * y + x + y + 0.1) * 0.25, 0))
-			new_cube, rigid_body = scene.add_physic_sphere(scn, world, width)
+			new_cube, rigid_body = scene.add_physic_sphere(scn, world, sphere_radius)
 			node_list.append(new_cube)
 			rb_list.append(rigid_body)
 
@@ -75,19 +73,19 @@ while not input.key_press(gs.InputDevice.KeyEscape) and not record_done:
 
 	if not thrown_bullet and dt_sum > 1.0:
 		world = gs.Matrix4.TransformationMatrix(make_solid_pos(-0.25,40), gs.Vector3())
-		new_cube, rigid_body = scene.add_physic_sphere(scn, world, width)
+		new_cube, rigid_body = scene.add_physic_sphere(scn, world, sphere_radius)
 		rigid_body.ApplyLinearImpulse(world.GetY() * -5)
 		node_list.append(new_cube)
 		rb_list.append(rigid_body)
 
 		world = gs.Matrix4.TransformationMatrix(make_solid_pos(-0.65,55), gs.Vector3())
-		new_cube, rigid_body = scene.add_physic_sphere(scn, world, width)
+		new_cube, rigid_body = scene.add_physic_sphere(scn, world, sphere_radius)
 		rigid_body.ApplyLinearImpulse(world.GetY() * -5)
 		node_list.append(new_cube)
 		rb_list.append(rigid_body)
 
 		world = gs.Matrix4.TransformationMatrix(make_solid_pos(-1.25,58), gs.Vector3())
-		new_cube, rigid_body = scene.add_physic_sphere(scn, world, width)
+		new_cube, rigid_body = scene.add_physic_sphere(scn, world, sphere_radius)
 		rigid_body.ApplyLinearImpulse(world.GetY() * -5)
 		node_list.append(new_cube)
 		rb_list.append(rigid_body)
@@ -124,7 +122,7 @@ f.write('#include "genesis.h"\n\n')
 f.write('#define SIMULATION_FRAME_LEN ' + str(len(stream_list)) + '\n')
 f.write('#define SIMULATION_NODE_LEN ' + str(len(node_list)) + '\n\n')
 
-f.write('const fix16 ' + 'physics_sim' + '[] =' + '\n')
+f.write('const s16 ' + 'physics_sim' + '[] =' + '\n')
 f.write('{\n')
 
 out_str = ''
@@ -137,7 +135,7 @@ for frame_record in stream_list:
 		out_str = '\t'
 
 	for node_record in frame_record:
-		out_str += "FIX16({0:.4f}".format(node_record['position'].x) + '), ' + "FIX16({0:.4f}".format(node_record['position'].y) + '), ' + "FIX16({0:.4f}".format(node_record['position'].z) + '), '
+		out_str += str(int(node_record['position'].x)) + ', ' + str(int(node_record['position'].y)) + ', '
 
 	out_str += '\n'
 	f.write(out_str)
