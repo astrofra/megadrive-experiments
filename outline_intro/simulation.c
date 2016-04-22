35ab8cb67f8122d4e7bf43ac35310b41abbdaf6e
astrofra <astrofra@gmail.com>
2016-04-22 21:31:26 +0200

    - Outline intro :   * multiple simulations working properly.

--- outline_intro/simulation.c
+++ outline_intro/simulation.c
@@ -1,13 +1,37 @@
 #include "genesis.h"
 #include <gfx.h>
 #include "simulation_0.h"
+#include "simulation_1.h"
 #include "transition_helper.h"
+
+#define MAX_SIMULATION 2;
 
 void RSE_physics_simulation(void)
 {
 	u32 vblCount = 0, i, j;
 	u16 vramIndex = TILE_USERINDEX;
 	Sprite sprites[80];
+	u8 current_scenario = 0;
+	s16 *physics_sim = NULL;
+	s16 sim_frame_len, sim_node_len;
+
+	void set_simulation(void)
+	{
+		switch(current_scenario)
+		{
+			case 0:
+				physics_sim = physics_sim_0;
+				sim_frame_len = SIMULATION_0_FRAME_LEN;
+				sim_node_len = SIMULATION_0_NODE_LEN;
+				break;
+
+			case 1:
+				physics_sim = physics_sim_1;
+				sim_frame_len = SIMULATION_1_FRAME_LEN;
+				sim_node_len = SIMULATION_1_NODE_LEN;
+				break;
+		}
+	}
 
 	RSE_turn_screen_to_black();
 
@@ -25,7 +49,7 @@ void RSE_physics_simulation(void)
 		// SPR_setFrame(&sprites[i], i%12);
 	}
 
-    SPR_update(sprites, SIMULATION_0_NODE_LEN);
+    SPR_update(sprites, sim_node_len);
 	VDP_setHilightShadow(0);
 
 	vramIndex = TILE_USERINDEX;
@@ -38,25 +62,29 @@ void RSE_physics_simulation(void)
 
 	SYS_enableInts();
 
-	// VDP_fadePalTo(PAL2, ball_metal.palette->data, 64, TRUE);
+	set_simulation();
 
 	while (TRUE)
 	{
 		VDP_waitVSync();
-		// BMP_showFPS(1);
 	
-		j = vblCount * SIMULATION_0_NODE_LEN * 3;
-		for(i = 0; i < SIMULATION_0_NODE_LEN;i++)
+		j = vblCount * sim_node_len * 3;
+		for(i = 0; i < sim_node_len;i++)
 		{
-			sprites[i].x = physics_sim_0[j++];
-			sprites[i].y = physics_sim_0[j++];
-			SPR_setFrame(&sprites[i], physics_sim_0[j++]);
+			sprites[i].x = physics_sim[j++];
+			sprites[i].y = physics_sim[j++];
+			SPR_setFrame(&sprites[i], physics_sim[j++]);
 		}
 
-		SPR_update(sprites, SIMULATION_0_NODE_LEN);
+		SPR_update(sprites, sim_node_len);
 
 		vblCount++;
-		if (vblCount >= SIMULATION_0_FRAME_LEN)
+		if (vblCount >= sim_frame_len)
+		{
 			vblCount = 0;
+			current_scenario++;
+			current_scenario %= MAX_SIMULATION;
+			set_simulation();
+		}
 	}
 }
